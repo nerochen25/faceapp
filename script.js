7ef4830ac0dd474974a27cde04b614af7677fb1d
Nero <nerochen25@berkeley.edu>
2020-09-07 17:19:09 -0700

    fix bug

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ Promise.all([
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+]).then(startVideo);
 
 function startVideo() {
-  navigator.mediaDevices.getUserMedia(
+  navigator.getUserMedia(
     { video: {} },
     stream => video.srcObject = stream,
     err => console.error(err)
