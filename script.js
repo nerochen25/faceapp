41d05fb5664a47075a3374a4e32fa9bc723187bd
Nero <nerochen25@berkeley.edu>
2020-09-07 17:43:46 -0700

    fix bug

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const video = document.getElementById('video');
 const expressionsList = document.getElementById('expressions-list');
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('./models')
 ]).then(startVideo).catch(err => console.log(err));
 
 function startVideo() {
